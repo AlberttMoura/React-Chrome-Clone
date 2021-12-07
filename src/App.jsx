37597947f55824d9e4e6ddae812b8f315b0162fe
AlberttMoura <BetinhoolXD@gmail.com>
2021-12-07 09:19:17 -0300

    Design Parte 1 - Button for switching to Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Routes} from './components/Routes'    
+import {Navbar} from './components/Navbar'
+import {Footer} from './components/Footer'
 
 const App = () => {
+    const [darkTheme, setDarkTheme] = useState('false')
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-200 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+                <Routes/>
+                <Footer/>
+            </div>
         </div>
     )
 }
